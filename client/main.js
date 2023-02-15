f8e7606579d6d2cbc055608617313506cf006bea
JOSE MANUEL DIAZ JIMENEZ <20191598@unapec.edu.do>
2023-02-14 23:53:23 -0400

    Loader ...

--- client/main.js
+++ client/main.js
@@ -7,5 +7,13 @@ const chatContainer = document.querySelector('#chat_container');
 let loadInterval
 
 function loader(element) {
-  '...'
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent === '....') {
+      element.textContent = '';
+    }
+  },300)
 }
